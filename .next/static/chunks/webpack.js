8fc8baa02970233b34a3c3eb29d3d5d8145d8aeb
AlexanderRebai <alexander.rebai@student.hogent.be>
2022-08-10 09:14:08 +0200

    added loading bar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c43ea9fffd65557"; }
+/******/ 		__webpack_require__.h = function() { return "b15e55a683e84e1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,35 @@
 import Layout from "../components/Layout";
+import Router from 'next/router'
 import "../styles/globals.css";
 import store from "../redux/store";
 import { Provider } from "react-redux";
+import NProgress from 'nprogress'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+
+  NProgress.configure({ showSpinner: false })
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  });
+
+
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Head>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
+      </Head>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   );
 }
 

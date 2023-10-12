a2b92ecd7d9c01301f418e604d3017a493f27459
Alexander Rebai <alexander-rebai@hotmail.com>
2023-10-12 16:20:22 +0200

    fix cart page height

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c7c3f038be1113e0"; }
+/******/ 		__webpack_require__.h = function() { return "c9bbae476beeb277"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
